40957a8e118c6f19924bd02f2d60b69b4edd0005
FedorTikhomirov <lord.nightmare.1234@gmail.com>
2024-12-11 22:20:57 +0100

    Restored CentralConsole functionality and added User class

---
+++ CentralConsole.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CentralConsole.aspx.cs" Inherits="HealthCentre.CentralConsole" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:Label ID="Label1" runat="server" Text="Search patient by ID: "></asp:Label><br />
+                <asp:TextBox ID="searchPatient" runat="server">PIN</asp:TextBox><asp:Button ID="searchButton" runat="server" Text="Search" OnClick="searchButton_Click" /><br />
+                <asp:Button ID="deleteButton" runat="server" Text="DELETE PATIENT" /><br /><br />
+
+                <asp:Label ID="Label2" runat="server" Text="Pin: "></asp:Label>
+                <asp:TextBox ID="pinEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label3" runat="server" Text="Email: "></asp:Label>
+                <asp:TextBox ID="emailEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label4" runat="server" Text="First name: "></asp:Label>
+                <asp:TextBox ID="firstEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label5" runat="server" Text="Last name: "></asp:Label>
+                <asp:TextBox ID="lastEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label6" runat="server" Text="Date of birth: "></asp:Label>
+                <asp:TextBox ID="dateEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label7" runat="server" Text="Address: "></asp:Label>
+                <asp:TextBox ID="addressEdit" runat="server"></asp:TextBox>
+                <br />
+
+                <asp:Label ID="Label8" runat="server" Text="Phone: "></asp:Label>
+                <asp:TextBox ID="phoneEdit" runat="server"></asp:TextBox>
+                <br /><br />
+
+                <asp:Button ID="saveButton" runat="server" Text="SAVE PATIENT INFO" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CentralConsole.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace HealthCentre {
+    public partial class CentralConsole : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            //if (Session["role"] != "Doctor") {
+            //    Response.Redirect("index.aspx");
+            //}
+        }
+
+        protected void searchButton_Click(object sender, EventArgs e) {
+            string pin = searchPatient.Text.Trim();
+
+            if(Regex.IsMatch(pin, "(^\\d{8}[A-Za-z]$)|(^[XYZ]\\d{7}[A-Za-z]$)\r\n")) {
+                string DBpath = Server.MapPath("~/data.db");
+                SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;");
+                conn.Open();
+
+                string query = "SELECT * FROM USERS WHERE pin = '" + pin + "';";
+                SQLiteCommand comm = new SQLiteCommand(query, conn);
+
+                SQLiteDataReader reader = comm.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                conn.Close();
+
+                if (dt.Rows.Count == 1) {
+                    DataRow row = dt.Rows[0];
+
+                    Dictionary<string, object> columnValues = new Dictionary<string, object>();
+                    foreach (DataColumn column in dt.Columns) {
+                        columnValues[column.ColumnName] = row[column.ColumnName];
+                    }
+
+                    User queriedUser = new User(
+                        Convert.ToInt32(columnValues["id"]),
+                        columnValues["pin"].ToString(),
+                        columnValues["password"].ToString(),
+                        columnValues["email"].ToString(),
+                        columnValues["role"].ToString(),
+                        columnValues["first_name"].ToString(),
+                        columnValues["last_name"].ToString(),
+                        columnValues["dob"].ToString(),
+                        columnValues["address"].ToString(),
+                        Convert.ToInt32(columnValues["phone"]),
+                        columnValues["created_at"].ToString(),
+                        columnValues["updated_at"].ToString()
+                    );
+
+                    pinEdit.Text = queriedUser.Pin;
+                    emailEdit.Text = queriedUser.Email;
+                    firstEdit.Text = queriedUser.First_Name;
+                    lastEdit.Text = queriedUser.Last_Name;
+                    dateEdit.Text = queriedUser.Dob;
+                    addressEdit.Text = queriedUser.Address;
+                    phoneEdit.Text = queriedUser.Phone.ToString();
+
+                }
+
+            }
+        }
+    }
+}
