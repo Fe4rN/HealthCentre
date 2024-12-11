d908d8e466e4d71defafb2ad6437d98942f5f463
FedorTikhomirov <lord.nightmare.1234@gmail.com>
2024-12-11 21:51:55 +0100

    Added Login functionality

---
+++ Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="HealthCentre.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="css/Login.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Please Log In to proceed to the console"></asp:Label><br />
+            <div class="LoginBox">
+                <asp:Label ID="Label2" runat="server" Text="Username: "></asp:Label>
+                <asp:TextBox ID="userInput" runat="server"></asp:TextBox>
+            </div>
+            <div class="LoginBox">
+                <asp:Label ID="Label3" runat="server" Text="Password: "></asp:Label>
+                <asp:TextBox ID="passInput" TextMode="Password" runat="server" ></asp:TextBox>
+            </div>
+            <asp:Button ID="LoginButton" runat="server" Text="LOG IN" OnClick="LoginButton_Click" />
+            <asp:Label ID="error" runat="server" Text="Username or password is not correct"></asp:Label>
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HealthCentre {
+    public partial class Login : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e) {
+            string pin = userInput.Text.Trim();
+            string password = passInput.Text.Trim();
+
+            //using (MD5 md5Hash = MD5.Create()) {
+            //    byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            //    password = BitConverter.ToString(data).Replace("-", string.Empty);
+            //}
+
+            string DBpath = Server.MapPath("~/data.db");
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;");
+            conn.Open();
+
+            string query = "SELECT * FROM USERS WHERE pin = '" + pin + "' AND password = '" + password + "';";
+            SQLiteCommand comm = new SQLiteCommand(query, conn);
+
+            SQLiteDataReader reader = comm.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+            conn.Close();
+
+            if (dt.Rows.Count == 1) {
+                String role = dt.Rows[0]["role"].ToString();
+                Session["pin"] = pin;
+                Session["role"] = role;
+
+                switch (role) {
+                    case "Patient":
+                        Response.Redirect("PatientHistory.aspx");
+                        break;
+
+                    case "Doctor":
+                        Response.Redirect("CentralConsole.aspx");
+                        break;
+
+                    default:
+                        //TO DO---
+                        break;
+                }
+            }
+            else {
+                error.Style["visibility"] = "visible";
+            }
+        }
+    }
+}
