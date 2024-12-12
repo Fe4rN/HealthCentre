cf48380e7e477cff2a29741103f6094719bf5fe7
Alex <addavid@edu.upv.es>
2024-12-12 02:15:13 +0100

    Patient v2

---
+++ PatientHistory.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PatientHistory.aspx.cs" Inherits="Healthcare_Alex_Fedor.PatientHistory" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>Patient History</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Patient Information</h1>
+        <p><strong>Name:</strong> <asp:Label ID="lblName" runat="server" Text="N/A"></asp:Label></p>
+        <p><strong>Date of Birth:</strong> <asp:Label ID="lblDOB" runat="server" Text="N/A"></asp:Label></p>
+        <p><strong>Address:</strong> <asp:Label ID="lblAddress" runat="server" Text="N/A"></asp:Label></p>
+        <p><strong>Phone:</strong> <asp:Label ID="lblPhone" runat="server" Text="N/A"></asp:Label></p>
+
+        <h2>Medical Records</h2>
+        <asp:GridView ID="gvRecords" runat="server" AutoGenerateColumns="true">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ PatientHistory.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Healthcare_Alex_Fedor
+{
+    public partial class PatientHistory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string DBpath = Server.MapPath("~/data.db");
+                using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;"))
+                {
+                    conn.Open();
+                    Response.Write("Connection Successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Database Connection Failed: " + ex.Message);
+            }
+
+            if (!IsPostBack)
+            {
+                int patientId = GetPatientIdFromQuery(); // Obtén el ID del paciente desde la URL u otro medio.
+                if (patientId > 0)
+                {
+                    LoadPatientInfo(patientId);
+                    LoadPatientRecords(patientId);
+                }
+            }
+        }
+
+        private int GetPatientIdFromQuery()
+        {
+            // Aquí suponemos que el ID del paciente viene como parámetro de querystring: ?patientId=1
+            string patientIdStr = Request.QueryString["patientId"];
+            int patientId;
+            return int.TryParse(patientIdStr, out patientId) ? patientId : 0;
+        }
+
+        private void LoadPatientInfo(int patientId)
+        {
+            string DBpath = Server.MapPath("~/data.db");
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;"))
+            {
+                conn.Open();
+                string query = "SELECT first_name, last_name, dob, address, phone FROM USERS WHERE id = @patientId";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@patientId", patientId);
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            lblName.Text = $"{reader["first_name"]} {reader["last_name"]}";
+                            lblDOB.Text = reader["dob"].ToString();
+                            lblAddress.Text = reader["address"].ToString();
+                            lblPhone.Text = reader["phone"].ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void LoadPatientRecords(int patientId)
+        {
+            string DBpath = Server.MapPath("~/data.db");
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;"))
+            {
+                conn.Open();
+                string query = "SELECT appointment_date, diagnosis, treatment, notes FROM RECORDS WHERE patient_id = @patientId";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@patientId", patientId);
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        gvRecords.DataSource = reader;
+                        gvRecords.DataBind();
+                    }
+                }
+            }
+        }
+    }
+}
